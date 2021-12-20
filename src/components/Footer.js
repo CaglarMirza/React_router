71574c9e8ab17cfe7fa25a686b24e5ea6021fc11
Caglar <ahmetcmirza@gmail.com>
2021-12-20 18:38:33 +0100

    footer?

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 const Footer = () => {
   return (
-    <footer className=" text-center bg-dark p-3 " style={{ marginTop: "30px" }}>
+    <footer
+      className=" text-center bg-dark p-3 navbar-fixed-bottom opacity-50"
+      style={{ marginTop: "30px" }}
+    >
       <p className="text-light">Copyright 2021</p>
     </footer>
   );
